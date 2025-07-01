884d6712619e9fd4de51df17002dcb030347628c
Hiccup <garciacassiohectorguillermo7@gmail.com>
2025-06-30 20:28:21 -0400

    create show.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+
+  root "products#index"
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -18,6 +21,6 @@ Rails.application.routes.draw do
 
   post "/products", to: "products#create"
   patch "/products/:id", to: "products#update"
-  
+
   delete "/products/:id", to: "products#destroy"
 end
